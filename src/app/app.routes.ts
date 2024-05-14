58b75942ce657091e913432a22ab48b15de181b9
nahidaRupta <nahida.rahaman37@gmail.com>
2024-05-14 23:47:51 +0600

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'login',pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {
+        path: 'budget-planner',
+        loadChildren: () => import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule)
+    }
+];
